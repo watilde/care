b6d33fc6b23ff666984cfa9c0562a0f1509d07ab
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-06-14 01:07:55 +0900

    Update init.js

--- example.js
+++ example.js
@@ -1,8 +1,10 @@
+/* CODE EXAMPLE */
+
 var care = require('./lib/index.js');
 
 var donation = care.init({
-  name: "Grunt",
-  description: "Your donation helps us continue Grunt development.",
+  name: "WWFジャパン",
+  description: "WWFは、人類が自然と調和して生きられる未来を目指し、\n約100カ国で活動している環境保全団体です。\n\nWWF ジャパンは、日本国内および日本が関係している国際的な問題に取り組みます。",
   email: 'robert.gravina-buyer@gmail.com',
 });
 
