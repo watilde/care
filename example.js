8690ba669d21a72b52ee9fb1fe257bce88fa35b5
Robert Gravina <robert.gravina@gmail.com>
2015-06-14 11:29:27 +0900

    Record payment from paypal.

--- example.js
+++ example.js
@@ -7,6 +7,8 @@ if (argv[2] !== 'donate') return;
 var care = require('./lib/index.js');
 
 var donation = care.init({
+  id: "wwf-japan",
+  repository: "grunt",
   name: "WWFジャパン",
   description: "WWFは、人類が自然と調和して生きられる未来を目指し、\n約100カ国で活動している環境保全団体です。\n\nWWF ジャパンは、日本国内および日本が関係している国際的な問題に取り組みます。",
   paypal: {
